4c742f9ef46d563491b70a6c85edfe8761d77d91
terratenff <49789537+terratenff@users.noreply.github.com>
2019-08-27 20:09:23 +0300

    Added tools to a designated toolbox

--- secondfloor/static/secondfloor/js/toolbox_functions/controllers.js
+++ secondfloor/static/secondfloor/js/toolbox_functions/controllers.js
@@ -3,30 +3,35 @@
  * Defines the controllers for the AngularJS Application "app".
  */
 
-// Sample Controller.
-app.controller('TestCtrl', function($scope, hexafy) {
+// Number Converter Controller.
+app.controller('NumberConverterCtrl', function($scope, baseShifter) {
     $scope.userInput = "";
+    $scope.userInputBaseA = 10;
+    $scope.userInputBaseB = 16;
     $scope.userOutput = "NaN";
+
+    $scope.baseA = function() {
+        baseShifter.setBaseA($scope.userInputBaseA);
+        $scope.userOutput = baseShifter.shift($scope.userInput);
+    }
+    $scope.baseB = function() {
+        baseShifter.setBaseB($scope.userInputBaseB);
+        $scope.userOutput = baseShifter.shift($scope.userInput);
+    }
     $scope.changed = function() {
-        $scope.userOutput = hexafy.myFunction($scope.userInput);
+        $scope.userOutput = baseShifter.shift($scope.userInput);
     }
 });
 
 // Toolbox Controller.
-app.controller('ToolboxCtrl', function($scope, hexafy) {
+app.controller('ToolboxCtrl', function($scope) {
     $scope.frontText = "Toolbox";
     $scope.visible = false;
     $scope.viewOptions = function() {
-        //$scope.frontText = "LUL";
-        console.log("CLICKED: " + $scope.visible);
         if ($scope.visible == true) {
-            console.log("PASS - BEFORE: " + $scope.visible);
             $scope.visible = false;
-            console.log("PASS - AFTER: " + $scope.visible);
         } else {
-            console.log("FAIL - BEFORE: " + $scope.visible);
             $scope.visible = true;
-            console.log("FAIL - AFTER: " + $scope.visible);
         }
     }
 });
--- secondfloor/static/secondfloor/js/toolbox_functions/services.js
+++ secondfloor/static/secondfloor/js/toolbox_functions/services.js
@@ -3,9 +3,55 @@
  * Defines various services for the AngularJS Application "app".
  */
 
-// Sample Service.
-app.service('hexafy', function() {
-    this.myFunction = function (x) {
-        return parseInt(x).toString(16);
+// Convert number of base A to number of base B.
+app.service('baseShifter', function() {
+    this.baseA = 10;
+    this.baseB = 16;
+    this.setBaseA = function(x) {
+        this.baseA = x;
+    }
+    this.setBaseB = function(x) {
+        this.baseB = x;
+    }
+    this.shift = function (x) {
+        return parseInt(x, this.baseA).toString(this.baseB);
+    }
+});
+
+// Encodes/Decodes text with Base64.
+app.service('base64', function() {
+    this.encode = function (x) {
+        return btoa(x);
+    }
+    this.decode = function (x) {
+        try {
+            return atob(x);
+        } catch(err) {
+            return "NaN";
+        }
+    }
+});
+
+// Manipulates text with Caesar Cipher.
+// Only considers alphabetical characters.
+app.service('caesar', function() {
+    this.alphabetReferenceLow = "abcdefghijklmnopqrstuvwxyz";
+    this.alphabetReferenceHigh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.shift = 3;
+    this.setShift = function(x) {
+        this.shift = x;
+    }
+    this.encode = function (x) {
+        var result = x;
+        for (var i = 0; i < result.length; i++) {
+            var charIndexLow = this.alphabetReferenceLow.indexOf(result[i]);
+            var charIndexHigh = this.alphabetReferenceLow.indexOf(result[i]);
+            if (charIndexLow !== -1) {
+                result[i] = this.alphabetReferenceLow[charIndexLow + this.shift % 26];
+            } else if (charIndexHigh !== -1) {
+                result[i] = this.alphabetReferenceHigh[charIndexHigh + this.shift % 26];
+            }
+        }
+        return result;
     }
 });
